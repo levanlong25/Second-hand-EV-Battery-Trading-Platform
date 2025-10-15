85dff3ff7947d2269ff1edb6cf7da215108d63d1
phamngocduong05 <duongpn4449@ut.edu.vn>
2025-10-15 14:03:49 +0700

    Create payment.py

---
+++ services/transaction-service/models/payment.py
@@ -0,0 +1,16 @@
+from app import db
+
+class Payment(db.Model):
+    __tablename__ = "payment"
+
+    payment_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    transaction_id = db.Column(db.Integer, db.ForeignKey('transaction.transaction_id'), nullable=False)
+    payment_method = db.Column(db.Enum('e-wallet', 'cash', 'bank'), nullable=False)
+    amount = db.Column(db.Float, nullable=False) # = final_price - fee
+    payment_status = db.Column(db.Enum('initiated', 'completed', 'failed'), default='initiated', nullable=False)
+    # completed = paid, failed = deleted transaction, initiated = pending
+
+    transaction = db.relationship('Transaction', back_populates='payment')
+
+    def __repr__(self):
+        return f"<Payment {self.payment_id} - Transaction {self.transaction_id} - Method {self.payment_method} - Status {self.payment_status}>"
